5b022e71988da38846e9babdccf9d1aa7af544d7
Arieda Muço <32225774+ariedamuco@users.noreply.github.com>
2019-11-24 18:52:29 +0100

    syntax consistency

--- code/pap2text.R
+++ code/pap2text.R
@@ -2,12 +2,9 @@ library(RCurl)
 library(XML)
 library(xml2)
 
-file<-read_html("pap.htm")
+file = read_html("pap.htm")
 doc = htmlParse(file, asText=TRUE)
 
-plain_text <- xpathSApply(doc, "//p", xmlValue)
-#cat_text=cat(paste(plain.text, collapse = "\n"))
+plain_text = xpathSApply(doc, "//p", xmlValue)
 
-text_write=write.csv(plain_text,file="pap.txt")
-
-
+text_write = write.csv(plain_text,file="pap.txt")
